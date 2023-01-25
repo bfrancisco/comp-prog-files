73ac3f707982ec37d44635607f774fb426f7649a
Bryan <franciscobryaaan@gmail.com>
2023-01-25 14:08:54 +0800

    abc185

--- ABC185/D.cpp
+++ ABC185/D.cpp
@@ -10,9 +10,26 @@ int main(){
     cin >> n >> m;
 
     ll a[200005];
-    for (int i = 0; i < m; i++){
-        cin 
+    a[0] = 0;
+    for (int i = 1; i <= m; i++){
+        cin >> a[i];
+    }
+    ll mn = 1e11;
+    sort(a+1, a+1+m);
+    a[m+1] = n;
+
+    for (int i = 0; i < m+1; i++){
+        mn = min(a[i+1] - a[i], mn);
+    }
+    cout << mn << endl;
+    ll ans = 0;
+    for (int i = 0; i < m+1; i++){
+        
+        ll val = ((a[i+1] - a[i]) + mn - 1) / mn;
+        cout << val << endl;
+        ans += val;
     }
 
+    cout << ans;
     return 0;
 }   
--- random_1.py
+++ random_1.py
@@ -1,2 +1,15 @@
-for _ in range(int(input())): 
-    s = input() ; print(int(s[0]) + int(s[-1]))
+for _ in range(int(input())):
+    n = int(input())
+    a = input()
+    out = []
+    val = int(a[0])
+    for i in range(1, n):
+        if val == 1:
+            out.append('-')
+        else:
+            out.append('+')
+
+        if a[i] == '1':
+            val = 1 - val
+            
+    print(''.join(out))

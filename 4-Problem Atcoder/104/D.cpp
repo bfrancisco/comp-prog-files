1a577eaac733ebcdf6f1ee469da3f64dcce9eed8
bfrancisco <franciscobryaaan@gmail.com>
2023-04-14 09:22:54 +0800

    abc104

---
+++ 4-Problem Atcoder/104/D.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// ABCBBC = 2 + 1 + 1
+// CCBABCAABBCC
+// 2 2 2
+// dp[5] = 1, {1, 1, 1}
+// dp[10] = , {2, 2, 1}
+// dp[11] = , {0, 0, 1}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    string s; cin >> s;
+    int n = s.size();
+
+    return 0;
+}

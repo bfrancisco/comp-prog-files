9c56a30a1d17ca75ffabb71587bb925a586c5a8e
bfrancisco <franciscobryaaan@gmail.com>
2023-08-24 18:08:13 +0800

    maxflowairpla

--- random2.py
+++ random2.py
@@ -1,7 +1,9 @@
-def gcd(a, b):
-    if (a == 0):
-        return b
-    return (gcd(b % a, a))
+sec = int(input("Seconds: "))
+day = sec//(24*60*60)
+sec -= day*(24*60*60)
+hour = sec//(60*60)
+sec -= hour*(60*60)
+mint = sec//60
+sec -= mint*60
 
-x, y = map(int, input().split())
-print(gcd(x, y))
+print(day, hour, mint, sec)

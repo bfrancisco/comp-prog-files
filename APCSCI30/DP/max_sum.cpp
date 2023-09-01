70a434b40245172cdf1e0de21d7d82eed7982770
bfrancisco <franciscobryaaan@gmail.com>
2023-09-01 23:58:32 +0800

    dp

--- APCSCI30/DP/max_sum.cpp
+++ APCSCI30/DP/max_sum.cpp
@@ -1,59 +1,64 @@
 // https://codeforces.com/gym/101853/problem/E
-
+// no dp yet
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-
+ 
 ll MOD = 1e9 + 7;
 ll mod(ll x, ll m){
     if (m == 0) return 0;
     if (m < 0) m *= -1;
     return (x%m + m) % m;
 }
-
+ 
 int n;
-int dp[16][(1 << 16) + 10];
+int dp[17][1 << 17];
 int rec(int i, int last_picks, int val, vector<vector<int>>& mat){
-    cout << i << " " << val << endl;
+    // cout << i << " " << val << endl;
     if (i == n) return val;
-    if (dp[i][last_picks] != -1) return dp[i][last_picks];
-    
-    dp[i][last_picks] = 0;
+    if (dp[i][last_picks] > 0) return dp[i][last_picks];
+
+    // int mx = 0;
     for (int bit = 0; bit < (1 << n); bit++){
         bool good = 1;
         int toadd = 0;
         for (int j = 0; j < n; j++){
             if ( (((bit & (1 << j)) && (bit & (1 << (j+1)))) ) || 
-                (bit & (1 << j) && (last_picks & (1 << j))) ||
-                (bit & (1 << j) && (last_picks & (1 << (j+1)))) ||
-                (bit & (1 << j) && j != 0 && (last_picks & (1 << (j-1))))){
+                (bit & (1 << j) && last_picks & (1 << j)) ||
+                (bit & (1 << j) && last_picks & (1 << (j+1))) ||
+                (bit & (1 << j) && j != 0 && last_picks & (1 << (j-1)))){
                     good = 0;
                     break;
                 }
             if (bit & (1 << j))
                 toadd += mat[i][n-j-1];
         }
-
-        if (good)
+ 
+        if (good){
             dp[i][last_picks] = max(dp[i][last_picks], rec(i+1, bit, val + toadd, mat));
+        }
     }
-
+ 
     return dp[i][last_picks];
 }
-
+ 
 int solve(){
     cin >> n;
     vector<vector<int>> mat(n, vector<int>(n));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             cin >> mat[i][j];
-    
-    memset(dp, -1, sizeof dp);
+
     int ret = rec(0, 0, 0, mat);
-    cout << dp[0][0] << endl;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < 1<<n; j++){
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
     return ret;
 }
-
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);

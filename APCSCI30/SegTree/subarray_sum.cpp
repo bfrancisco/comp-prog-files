e7191f6f600306f79e81d75650fc983789c1fad0
bfrancisco <franciscobryaaan@gmail.com>
2023-09-19 07:08:05 +0800

    segt

--- APCSCI30/SegTree/subarray_sum.cpp
+++ APCSCI30/SegTree/subarray_sum.cpp
@@ -3,18 +3,20 @@ using namespace std;
 typedef long long int ll;
 
 struct segtree {
-    int n, *vals, *deltas;
-    segtree(vector<int> &ar) {
+    int n; ll *vals, *posvals, *deltas;
+    segtree(vector<ll> &ar) {
         n = ar.size();
-        vals = new int[4*n];
-        deltas = new int[4*n];
+        vals = new ll[4*n];
+        deltas = new ll[4*n];
+        posvals = new ll[4*n];
         build(ar, 1, 0, n-1);
     }
     
-    void build(vector<int> &ar, int p, int i, int j) {
+    void build(vector<ll> &ar, int p, int i, int j) {
         deltas[p] = 0;
         if (i == j) {
             vals[p] = ar[i];
+            posvals[p] = max(0ll, ar[i]);
         }
         else {
             int k = (i + j) / 2;
@@ -26,11 +28,13 @@ struct segtree {
     
     void pull(int p) {
         vals[p] = max(vals[p<<1], vals[p<<1|1]);
+        posvals[p] = max(0ll, vals[p]);
     }
 
     void push(int p, int i, int j) {
-        if (deltas[p]) { 
-            vals[p] += (j - i + 1) * deltas[p];
+        if (deltas[p]) {
+            vals[p] += deltas[p];
+            posvals[p] = max(0ll, vals[p]);
             if (i != j) {
                 deltas[p<<1] += deltas[p];
                 deltas[p<<1|1] += deltas[p];
@@ -39,8 +43,21 @@ struct segtree {
         }
     }
 
+    void ulti_push(int p, int i, int j, int target){
+        // cout << p << endl;
+        push(p, i, j);
+        if (i == j) return;
+        int k = (i + j) / 2;
+        if (target <= k){
+            ulti_push(p<<1, i, k, target);
+        }
+        else{
+            ulti_push(p<<1|1, k+1, j, target);
+        }
+    }
+
     // i, j starts at 0, n-1
-    void update(int _i, int _j, int v, int p, int i, int j) {
+    void update(int _i, int _j, ll v, int p, int i, int j) {
         push(p, i, j);
         // query overlaps or equates i, j
         if (_i <= i && j <= _j) {
@@ -56,19 +73,6 @@ struct segtree {
             pull(p);
         }
     }
-
-    int query(int _i, int _j, int p, int i, int j) {
-        push(p, i, j);
-        if (_i <= i && j <= _j)
-            return vals[p];
-        else if (_j < i || j < _i)
-            return 0;
-        else {
-            int k = (i + j) / 2;
-            return  query(_i, _j, p<<1, i, k) +
-                    query(_i, _j, p<<1|1, k+1, j);
-        }
-    }
 };
 
 int main(){
@@ -76,19 +80,48 @@ int main(){
     cin.tie(NULL); cout.tie(NULL);
     
     int n, q; cin >> n >> q;
-    vector<ll> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
 
+    vector<int> n2(20);
+    n2[0] = 1;
+    for (int i = 1; i <= 20; i++){
+        n2[i] = n2[i-1] * 2;
+    }
+    int newN = n2[lower_bound(n2.begin(), n2.end(), n) - n2.begin()];
+
+    vector<ll> a(newN);
+    vector<ll> p(newN);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = n; i < newN; i++)
+        a[i] = 0;
+
+    p[0] = max(0ll, a[0]);
+    for (int i = 1; i < n; i++)
+        p[i] = max(0ll, a[i] + p[i-1]);
+    for (int i = n; i < newN; i++)
+        p[i] = max(0ll, p[i-1]);
+
+    for (auto e : p) cout << e << " "; cout << endl;
+    segtree seg(p);
+    // for (int i = 1; i < newN*2; i++) cout << seg.vals[i] << " "; cout << endl;
     while(q--){
         int k; ll v; cin >> k >> v;
-        seg.update(k-1, v);
-        ll mx = -1e18, cur = 0;
-        for (int i = 0; i < n; i++){
-            cur += seg.vals[i+newN];
-            if (mx < cur) mx = cur;
-            if (cur < 0) cur = 0;
+        k--;
+        ll diff = v - a[k];
+        a[k] = v;
+        if (seg.vals[k+newN] + diff >= 0){
+            seg.update(k, newN-1, diff, 1, 0, newN-1);
         }
-        cout << max(mx, 0ll) << endl;
+        else {
+            seg.ulti_push(1, 0, newN-1, k);
+            // cout << -seg.posvals[k+newN] << endl;
+            seg.update(k+1, newN-1, -seg.posvals[k+newN], 1, 0, newN-1);
+            seg.update(k, k, diff, 1, 0, newN-1);
+        }
+        cout << seg.posvals[1] << endl;
+        // for (int i = 1; i < newN*2; i++) cout << seg.vals[i] << " "; cout << endl;
+
     }
 
     return 0;
---
+++ random2.py
@@ -0,0 +1,9 @@
+robin = {
+    "stats" : {
+        "hp" : 1,
+        "mana" : 2
+    }
+}
+
+print(robin["stats"])
+print(robin["stats"]["mana"])

018cb1509ed00df455ee085f8f62f411a53553ce
bfrancisco <franciscobryaaan@gmail.com>
2023-10-02 08:15:27 +0800

    serej

--- APCSCI30/Amortized/rectangle2.cpp
+++ APCSCI30/Amortized/rectangle2.cpp
@@ -6,8 +6,33 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
+    ll n; cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+        cin >> a[i];
     
+    vector<ll> s; // stores index
+    ll i = 0;
+    ll ans = 0;
+    while (i < n){
+        if (s.empty() || a[s.back()] <= a[i]){
+            s.push_back(i);
+            i++;
+        }
+        else{
+            ll j = s.back();
+            s.pop_back();
+            ans = max(ans, (!s.empty() ? (i - s.back() - 1) * a[j] : i * a[j]));
+        }
+    }
 
+    while (!s.empty()){
+        ll j = s.back();
+        s.pop_back();
+        ans = max(ans, (!s.empty() ? (n - s.back() - 1) * a[j] : n * a[j]));
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
---
+++ APCSCI30/Amortized/sereja.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int n, m, p;
+    cin >> n >> m >> p;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+
+    map<int, int> valtoinds;
+    map<int, int> bmap;
+    int count = 0;
+    vector<int> qs;
+    for (int i = 0; i < m; i++){
+        valtoinds[b[i]] = 0;
+        if (bmap.find(b[i]) == bmap.end())
+            bmap[b[i]] = 1;
+        else
+            bmap[b[i]]++;
+    }
+
+    for (int i = 0; i < m; i++){
+        if (bmap.find(a[i]) != bmap.end()){
+            valtoinds[a[i]]++;
+            if (bmap[a[i]] == valtoinds[a[i]]){
+                count++;
+            }
+        }
+    }
+    if (count == m) qs.push_back(1);
+
+    for (int i = 0, j = m; j < n; i++, j++){
+        // addr
+        if (bmap.find(a[j]) != bmap.end()){
+            valtoinds[a[j]]++;
+            if (bmap[a[j]] == valtoinds[a[j]]){
+                count++;
+            }
+        }
+
+        // popl
+        if (bmap.find(a[i]) != bmap.end()){
+            valtoinds[a[i]]--;
+            if (valtoinds[a[i]] == bmap[a[i]] - 1){
+                count--;
+            }
+        }
+
+        if (count == m) qs.push_back(i+2);
+    }
+
+    cout << qs.size() << endl;
+    for (auto e : qs) cout << e << " "; cout << endl;
+
+    return 0;
+}
+
+// 1 2 3 4 5 6 7 8 9 10
+// 1 2 3 4 5 

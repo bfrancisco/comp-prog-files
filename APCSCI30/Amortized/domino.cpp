664418e5017e50fc197f712fb87ccda2dc21228d
bfrancisco <franciscobryaaan@gmail.com>
2023-10-04 10:49:34 +0800

    amortized finished

--- APCSCI30/Amortized/domino.cpp
+++ APCSCI30/Amortized/domino.cpp
@@ -2,6 +2,55 @@
 using namespace std;
 typedef long long int ll;
 
+struct segtree{
+    int n; int *mx;
+    segtree(vector<int>& ar){
+        n = ar.size();
+        mx = new int[4*n];
+        for (int i = 0; i < 4*n; i++){
+            mx[i] = -1e9;
+        }
+        build(ar, 1, 0, n-1);
+    }
+
+    void build(vector<int>& ar, int p, int i, int j){
+        if (i == j){
+            mx[p] = ar[i];
+        }
+        else{
+            int k = (i + j) / 2;
+            build(ar, p << 1, i, k);
+            build(ar, p << 1 | 1, k+1, j);
+            mx[p] = max(mx[p<<1], mx[p<<1|1]);
+        }
+    }
+
+    int query(int l, int r, int p, int i, int j){
+        if (l <= i && j <= r)
+            return mx[p];
+        else if (r < i || j < l)
+            return -1e9;
+        else{
+            int k = (i + j) / 2;
+            return max(query(l, r, p << 1, i, k), query(l, r, p << 1 | 1, k+1, j));
+        }
+    }
+
+    // point
+    void update(int ind, int v, int p, int i, int j){
+        if (ind == i && j == ind)
+            mx[p] = v;
+        else if (ind < i || j < ind)
+            return;
+        else{
+            int k = (i + j) / 2;
+            update(ind, v, p<<1, i, k);
+            update(ind, v, p<<1|1, k+1, j);
+            mx[p] = max(mx[p<<1], mx[p<<1|1]);
+        }
+    }
+};
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
@@ -9,31 +58,32 @@ int main(){
     int n; cin >> n;
     vector<pair<int, int>> a(n);
     map<pair<int, int>, int> atoi;
-    map<int, pair<int, int>> itoa;
     for (int i = 0; i < n; i++){
         int x, h; cin >> x >> h;
         a[i] = {x, h};
         atoi[a[i]] = i;
-        itoa[i] = a[i];
     }
 
     sort(a.begin(), a.end());
 
-    vector<int> fall(n);
-    fall[n-1] = 1;
-    for (int i = n-2; i >= 0; i--){
-        int val = a[i].first + a[i].second;
+    vector<int> far(n), fall(n);
+    for (int i = 0; i < n; i++){
+        far[i] = a[i].first + a[i].second - 1;
+    }
 
-        if (a[i+1].first+a[i+1].second >= val && val-1 >= a[i+1].first){
-            fall[i] = fall[i+1] + 1;
-            continue;
-        }
+    segtree segfar(far);
 
-        int j = (lower_bound(a.begin()+i, a.end(), make_pair(val, 0))) - a.begin();
+    for (int i = n-1; i >= 0; i--){
+        int r = upper_bound(a.begin()+i, a.end(), make_pair(far[i], (int)1e9)) - a.begin() - 1;
+        int mx = segfar.query(i, r, 1, 0, n-1);
+        segfar.update(i, mx, 1, 0, n-1);
+        // cout << i << " " << r << endl;
+        int j = upper_bound(a.begin()+i, a.end(), make_pair(mx, (int)1e9)) - a.begin();
         fall[i] = j-i;
     }
 
-    for (auto e : fall) cout << e << " "; cout << endl;
+    // for (auto e : far) cout << e << " "; cout << endl;
+    // for (auto e : fall) cout << e << " "; cout << endl;
 
     vector<int> ans(n);
     for (int i = 0; i < n; i++){
@@ -42,7 +92,7 @@ int main(){
 
     for (int i = 0; i < n; i++)
         cout << ans[i] << " ";
-    cout << endl; 
+    cout << endl;
 
     return 0;
 }

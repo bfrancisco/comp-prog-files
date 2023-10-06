379397dcc47ad90cc93a9e167747c16e65943960
bfrancisco <franciscobryaaan@gmail.com>
2023-10-06 12:35:38 +0800

    fix

--- Upsolves/jcpc2022-H.cpp
+++ Upsolves/jcpc2022-H.cpp
@@ -1,15 +1,13 @@
+// https://codeforces.com/gym/104147/problem/H
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
 
-struct{
-    bool operator()(int a, int b) const { return abs(a) < abs(b); }
-}
-abscomp;
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    
     
     
     return 0;
--- stress-test.py
+++ stress-test.py
@@ -1,3 +1,5 @@
+import random
+
 def generate():
     "generate random test case here"
 

79967324e1b757dbc3e25c2f30185f85e08869e8
bfrancisco <franciscobryaaan@gmail.com>
2023-10-16 10:27:09 +0800

    knaposack2

--- APCSCI30/DPoptimization/knapsack2.cpp
+++ APCSCI30/DPoptimization/knapsack2.cpp
@@ -2,28 +2,24 @@
 using namespace std;
 typedef long long int ll;
 
-const int MXN = 100;
-const int MXV = 100;
-ll dp[MXN+1][MXN*MXV+1];
+const ll MXN = 100;
+const ll MXV = 1000;
+// ll dp[MXN+10][MXN*MXV+10];
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    int n; ll w; cin >> n >> w;
-    vector<ll> W(n), V(n);
+    ll n; ll w; cin >> n >> w;
+    vector<ll> W(n+1), V(n+1);
     for (int i = 1; i <= n; i++){
         cin >> W[i] >> V[i];
     }
 
-    for (int i = 0; i <= MXN; i++){
-        for (int j = 0; j <= n*MXV; j++){
-            dp[i][j] = 1e15;
-        }
-    }
+    vector<vector<ll>> dp(110, vector<ll>(100010, (ll)1e18));
     dp[0][0] = 0;
     for (int i = 1; i <= n; i++){
-        for (int j = 0; j <= n*MXV; j++){
+        for (ll j = 0; j <= n*MXV; j++){
             if (j - V[i] < 0)
                 dp[i][j] = dp[i-1][j];
             else
@@ -31,8 +27,8 @@ int main(){
         }
     }
 
-    int ans = 1e9;
-    for (int val = n*MXV; val >= 0; val--){
+    ll ans;
+    for (ll val = n*MXV; val >= 0; val--){
         // cout << val << " " << dp[n][val] << endl;
         if (dp[n][val] <= w){
             ans = val;

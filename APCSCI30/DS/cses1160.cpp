e4cb35a1f4341f4b8a29a59d99bcf26e6c16d115
bfrancisco <franciscobryaaan@gmail.com>
2023-11-01 15:32:16 +0800

    apcsci ds

---
+++ APCSCI30/DS/cses1160.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ 
+int lift[200005][31];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int n, l;
+    int q;
+    cin >> n >> q;
+    l = 30; // log2(1e9)
+ 
+    for (int u = 1; u <= n; u++){
+        cin >> lift[u][0];
+    }
+    
+    for (int j = 1; j < l; j++){
+        for (int u = 1; u <= n; u++){
+            lift[u][j] = lift[lift[u][j-1]][j-1];
+        }
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << lift[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    // while (q--){
+        
+    // }
+    return 0;
+}

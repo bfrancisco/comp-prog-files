9ed85c11ce86aaba28289a1d67bc88ac86808dbb
Bryan Francisco <franciscobryaaan@gmail.com>
2023-11-13 19:58:56 +0800

    justkomit

--- random.cpp
+++ random.cpp
@@ -6,8 +6,13 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    cout << sqrt(0)-1 << endl;
-
+    for (int x = 1; x <= 1060; x++){
+        if (x % 11 == 9 and
+            x % 14 == 13 and
+            x % 9 == 7){
+            cout << x << endl;
+        }
+    }
 
     return 0;
 }

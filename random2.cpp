f1c0c3ce114588ddf0e059b799d1adf140f2d6a1
bfrancisco <franciscobryaaan@gmail.com>
2023-11-29 16:51:41 +0800

    kungano

--- random2.cpp
+++ random2.cpp
@@ -1,30 +1,28 @@
 #include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 typedef long long int ll;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    ordered_set;
+// cses 1683
 
+vector<vector<int>> adj;
+vector<vector<int>> adj, sccs;
+vector<int> disc, low, stck;
+int ti = 0;
+int dfs(int u){
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < m; i++){
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+    }
 
-    ordered_set s;
-    s.insert(1);
-    s.insert(9);
-    s.insert(5);
-    s.insert(3);
-    s.insert(7);
-
-    cout << s.order_of_key(5) << endl;
-    cout << s.order_of_key(7) << endl;
-    cout << s.order_of_key(0) << endl;
-    cout << s.order_of_key(10) << endl;
+    disc.assign(n, -1);
+    low.assign(n, -1);
 
     return 0;
 }

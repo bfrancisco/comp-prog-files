feb66dc46c1f57ffd4df41599a1b63a797ec5c2d
bfrancisco <franciscobryaaan@gmail.com>
2023-12-15 01:53:09 +0800

    aa

---
+++ APCSCI30/Strings/cf113b.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    string s, a, b; cin >> s >> a >> b;
+    
+
+    return 0;
+}

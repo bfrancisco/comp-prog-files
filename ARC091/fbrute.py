60a15d6a33e6afdf7f19925e0967b6bf8c49975f
bfrancisco <franciscobryaaan@gmail.com>
2024-01-22 20:01:13 +0800

    arc091

---
+++ ARC091/fbrute.py
@@ -0,0 +1,18 @@
+def grundy(a, k):
+    s = []
+    for v in range(1, a//k + 1):
+        s.append(grundy(a-v, k))
+    s.sort()
+    ret = 0
+    for e in s:
+        if e != ret:
+            break
+        ret+=1
+    return ret
+
+n = int(input()) 
+ans = 0
+for i in range(n):
+    a, k = map(int, input().split())
+    ans ^= grundy(a, k)
+print("Takahashi" if ans else "Aoki")
--- ARC091/fhelp.py
+++ ARC091/fhelp.py
@@ -1,21 +1,25 @@
-def rec(a, k, c):
-    global mn
-    global mx
-    global st
+N = 201
+dp = [-1 for i in range(N)]
 
-    if (a/k < 1):
-        mn = min(mn, c)
-        mx = max(mx, c)
-        st.add(c)
-
+def grundy(a, k):
+    if (dp[a] != -1):
+        return dp[a]
+    
+    s = []
     for v in range(1, a//k + 1):
-        rec(a-v, k, c+1)    
+        s.append(grundy(a-v, k))
+    s.sort()
+    ret = 0
+    for e in s:
+        if e != ret:
+            break
+        ret+=1
+    dp[a] = ret
+    return ret
 
-a, k = map(int, input().split())
-mn, mx = 1000000, -10000000
-st = set()
-rec(a, k, 0)
-
-print(mn, mx)
-print(st)
-
+# a, k = map(int, input().split())
+k = 3
+for a in range(1, N):
+    g = grundy(a, k)
+    if a%3:
+        print(f"{a}\t:\t{g}")

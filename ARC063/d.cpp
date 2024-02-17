497f9acdc0871bf9b9d096a8ea727aa7365d1d47
bfrancisco <franciscobryaaan@gmail.com>
2024-02-17 10:33:05 +0800

    arc063

---
+++ ARC063/d.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, a, b; cin >> n >> a >> b;
+    vector<int> monsters(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    
+
+    return 0;
+}

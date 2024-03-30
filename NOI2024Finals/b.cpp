582b5c9cb8cd610ceda26f0cb0f02b1507ce57f5
bfrancisco <franciscobryaaan@gmail.com>
2024-03-30 19:51:43 +0800

    NOI Finals 2024 upsolve

--- NOI2024Finals/b.cpp
+++ NOI2024Finals/b.cpp
@@ -2,13 +2,8 @@
 using namespace std;
 typedef long long int ll;
 
-vector<vector<int>> grid;
-
-int f(int a, int b, int c, int d){
-    int x = abs(a-c), y = abs(b-d);
-    if (x > y) swap(x, y);
-    return x + (y-x);
-}
+int n;
+vector<int> contrib;
 
 int main(){
     ios_base::sync_with_stdio(false);

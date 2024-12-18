5dd6c6cca91edfca0dd0524a731c019b64dbb346
bfrancisco <franciscobryaaan@gmail.com>
2024-12-18 20:14:36 +0800

    div2c

--- random2.py
+++ random2.py
@@ -1,11 +1,28 @@
 for _ in range(int(input())):
     n = int(input())
-    s = input()
-    ans = "NO"
-    for i in range(n-1):
-        if s[i] == s[i+1] and s[i] == '1':
-            ans = "YES"
-            break
-    if s[0] == '1' or s[-1] == '1':
-        ans = "YES"
-    print(ans)
+
+    if n%2==0:
+        for i in range(n//2):
+            print(i+1, i+1, end=' ')
+        print()
+    
+    elif n >= 27:
+        i = 1
+        cnt = 0
+        prev = 0
+        while i <= n:
+            if i == 1 or i == 10 or i == 26:
+                print(1, end=' ')
+            elif i == 23 or i == 27:
+                print(10**6, end=' ')
+            elif cnt == 0:
+                print(i, end=' ')
+                prev = i
+                cnt = 1 - cnt
+            elif cnt == 1:
+                print(prev, end=' ')
+                cnt = 1 - cnt
+            i += 1
+    
+    else:
+        print(-1)

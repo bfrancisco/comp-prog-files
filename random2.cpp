cbf88a931f463a317414e069805b485d3b423bc3
bfrancisco <franciscobryaaan@gmail.com>
2025-01-02 11:02:07 +0800

    random

--- random2.cpp
+++ random2.cpp
@@ -1,138 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
+using ll = long long int;
 
-// i, 2 left top, 2 left bottom
-int dp[100005][2][2][2][2];
-vector<vector<char>> board;
-int n;
+ll process(int R, int C, vector<vector<ll>>& grid){
+    map<int, vector<ll>> db;
 
-// a: top left, b: bottom left, top right, bottom right
-// 1 if used, else 0
-int rec(int i, int a, int b, int c, int d){
-    if (i > n) return 0;
-    if (dp[i][a][b][c][d] != -1) 
-        return dp[i][a][b][c][d];
-    
-    int ans = 0, alice = 0;
-
-    if (a+b+c+d == 0){
-        int wins = 0;
-        for (int r = 0; r < 2; r++){
-            alice = 0;
-            for (int c = i; c >= i-2; c--){
-                alice += (board[r][c] == 'A');
-            }
-            if (alice >= 2) wins++;
+    for (int r = 0; r < R; r++){
+        for (int c = 0; c < C; c++){
+            db[grid[r][c]].push_back(c);
         }
-        ans = max(ans, rec(i+1, 1, 1, 1, 1) + wins);
     }
 
-    else if (a && b+c+d == 0){
-        alice = 0;
-        for (int c = i-2; c <= i; c++)
-            alice += (board[1][c] == 'A');
-        ans = max(ans, rec(i+1, 0, 1, 0, 1) + (alice >= 2));
+    for (auto& [color, vc] : db){
+        sort(db[color].begin(), db[color].end(), greater<ll>());
     }
 
-    else if (b && a+c+d == 0){
-        alice = 0;
-        for (int c = i-2; c <= i; c++)
-            alice += (board[0][c] == 'A');
-        ans = max(ans, rec(i+1, 1, 0, 1, 0) + (alice >= 2));
+    ll ret = 0;
+    for (auto& [color, vc] : db){
+        // cout << "Color: " << color << endl;
+
+        int sz = vc.size();
+        if (sz == 1) continue;
+
+        // for (auto e : vc) cout << e << " "; cout << endl; 
+
+        for (int i = 0, mul = sz-1; i < sz/2; i++, mul -= 2){
+            ret += vc[i] * mul;
+            // cout << "\t" << vc[i] << " * " << mul << " = " << vc[i] * mul << endl;
+        }
+        // cout << "neg" << endl;
+        for (int i = sz/2, mul = (sz%2 ? 0 : 1); i < sz; i++, mul += 2){
+            ret -= vc[i] * mul;
+            // cout << "\t" << vc[i] << " * " << mul << " = " << vc[i] * mul << endl;
+
+        }
     }
 
-    else if (a+c == 2 && b+d == 0){
-        alice = 0;
-        for (int c = i-2; c <= i; c++)
-            alice += (board[1][c] == 'A');
-        ans = max(ans, rec(i+1, 1, 1, 0, 1) + (alice >= 2)); 
-    }
-
-    else if (a+c == 0 && b+d == 2){
-        alice = 0;
-        for (int c = i-2; c <= i; c++)
-            alice += (board[0][c] == 'A');
-        ans = max(ans, rec(i+1, 1, 1, 1, 0) + (alice >= 2));
-    }
-
-    else if (a+b == 2 && c+d == 0){
-        // top right, 2 left
-        alice = 0;
-        alice += (board[0][i] == 'A');
-        alice += (board[0][i-1] == 'A');
-        alice += (board[1][i-1] == 'A');
-        
-        ans = max(ans, rec(i+1, 1, 1, 1, 0) + (alice >= 2));
-
-        // bottom right, 2 left
-        alice = 0;
-        alice += (board[1][i] == 'A');
-        alice += (board[0][i-1] == 'A');
-        alice += (board[1][i-1] == 'A');
-
-        ans = max(ans, rec(i+1, 1, 1, 0, 1) + (alice >= 2));
-    }
-
-    else if (d == 0 && a+b+c == 3){
-        alice = 0;
-        alice += (board[1][i-1] == 'A');
-        alice += (board[0][i] == 'A');
-        alice += (board[1][i] == 'A');
-
-        ans = max(ans, rec(i+1, 1, 1, 1, 1) + (alice >= 2));
-    }
-    
-    else if (c == 0 && a+b+d == 3){
-        alice = 0;
-        alice += (board[0][i-1] == 'A');
-        alice += (board[0][i] == 'A');
-        alice += (board[1][i] == 'A');
-
-        ans = max(ans, rec(i+1, 1, 1, 1, 1) + (alice >= 2));
-    }
-
-    if (a+b == 2){
-        ans = max(ans, rec(i+1, c, d, 0, 0));
-    }
-
-
-    dp[i][a][b][c][d] = ans;
-
-    return ans;
+    // cout << "ret: " << ret << endl;
+    return ret;
 }
 
 void solve(){  
-    cin >> n;
-    board.assign(2, vector<char>(n+1, '-'));
-    for (int z = 0; z < 2; z++){
-        for (int i = 1; i <= n; i++){
-            cin >> board[z][i];
+    int R, C;
+    cin >> R >> C;
+    vector<vector<ll>> grid(vector<vector<ll>>(R, vector<ll>(C, 0)));
+    for (int r = 0; r < R; r++){
+        for (int c = 0; c < C; c++){
+            cin >> grid[r][c];
         }
     }
 
-    for (int i = 0; i <= n; i++){
-        for (int a = 0; a < 2; a++)
-        for (int b = 0; b < 2; b++)
-        for (int c = 0; c < 2; c++)
-        for (int d = 0; d < 2; d++)
-            dp[i][a][b][c][d] = -1;
+    vector<vector<ll>> rev(vector<vector<ll>>(C, vector<ll>(R, 0)));
+    for (int r = 0; r < R; r++){
+        for (int c = 0; c < C; c++){
+            rev[c][r] = grid[r][c];
+        }
     }
 
-    // for (auto r : board){
-    //     for (auto e : r) cout << e << " "; cout << endl;
-    // }
+    ll ans = 0;
+    ans += process(R, C, grid);
+    ans += process(C, R, rev);
 
-    cout << rec(2, 1, 1, 0, 0) << endl;
-    
-
+    cout << ans << endl;
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    int t; cin >> t;
+    int t = 1;
+    // int t; cin >> t;
     while (t--){
         solve();
     }
--- random2.py
+++ random2.py
@@ -1,16 +1,11 @@
-for _ in range(int(input())):
-    n, m = map(int, input().split())
-    R = [n for i in range(n+1)]
-    for i in range(m):
-        x, y = map(int, input().split())
-        if x > y: x, y = y, x
-        R[x] = min(R[x], y-1)
-    
-    for i in range(n-1, 0, -1):
-        R[i] = min(R[i], R[i+1])
-    
-    ans = 0
-    for i in range(1, n+1):
-        ans += R[i] - i + 1
-    
-    print(ans)
+s = input()
+n = len(s)
+
+for rpad in range(n):
+    pattern = s[:rpad]
+    for lrep in range(1, min(rpad, n-len(pattern)+1)):
+        if pattern == s[lrep:lrep+len(pattern)] and lrep+len(pattern) == n:
+            print("YES")
+            print(pattern)
+            exit()
+print("NO")

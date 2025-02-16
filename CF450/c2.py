cfb601416479d6a79c9f0424cf90cf04486ce069
bfrancisco <franciscobryaaan@gmail.com>
2025-02-16 22:20:07 +0800

    cf450 upsolve

--- CF450/c2.py
+++ CF450/c2.py
@@ -1,4 +1,21 @@
 n = int(input())
-a = [int(i) for i in input().split()]
+A = [int(i) for i in input().split()]
+H = [0 for i in range(n)]
+high = 0
+for i in range(n):
+    high = max(high, A[i])
+    H[i] = high
 
-high 
+mp = {}
+for h in H:
+    if h not in mp: mp[h] = 1
+    else: mp[h] += 1
+mx = max(mp.values())
+ans = 1e9
+for k, v in mp.items():
+    if v == mx:
+        ans = min(ans, k)
+
+print(*H)
+print(mp)
+print(ans)

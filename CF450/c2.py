72f72a08a9b734010bbd4658080966fb9a52194e
bfrancisco <franciscobryaaan@gmail.com>
2025-02-18 15:57:27 +0800

    cf450

--- CF450/c2.py
+++ CF450/c2.py
@@ -1,21 +1,40 @@
 n = int(input())
 A = [int(i) for i in input().split()]
-H = [0 for i in range(n)]
-high = 0
+high2 = [[0, 0] for i in range(n)] # high[0] > high[1]
+contrib = [0 for i in range(n+1)]
+
+high2[0][0] = A[0]
+contrib[A[0]] = -1
+for i in range(1, n):
+    high2[i][0] = high2[i-1][0]
+    high2[i][1] = high2[i-1][1]
+    if A[i] > high2[i][0]:
+        high2[i] = [A[i], high2[i][0]]
+    elif A[i] > high2[i][1]:
+        high2[i][1] = A[i]
+    
+    if high2[i][0] == A[i]:
+        contrib[A[i]] = -1
+
+print(contrib)
+
+freq = [0 for i in range(n+1)]
 for i in range(n):
-    high = max(high, A[i])
-    H[i] = high
+    if A[i] in high2[i]:
+        freq[high2[i][0]] += 1
 
-mp = {}
-for h in H:
-    if h not in mp: mp[h] = 1
-    else: mp[h] += 1
-mx = max(mp.values())
-ans = 1e9
-for k, v in mp.items():
-    if v == mx:
-        ans = min(ans, k)
+for i in range(n):
+    freq[high2[i][0]] = max(freq[A[i]]-1, 0)
+    contrib[high2[i][0]] += freq[high2[i][0]]
 
-print(*H)
-print(mp)
+mx = max(contrib)
+ans = 0
+for i in range(1, n+1):
+    if mx == contrib[i]:
+        ans = i
+        break
+
+print(*high2)
+print(contrib)
 print(ans)
+    
--- template.cpp
+++ template.cpp
@@ -1,11 +1,13 @@
-#include<bits/stdc++.h>
+#include<stdio.h>
 using namespace std;
-typedef long long int ll;
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
     
-    
+    int x = 5;
+    int y = 3;
+    int z = x+y;
+
     return 0;
 }
